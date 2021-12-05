2e651e2c113995790385a75b7329312547e80338
Bhuttu03@ <sambit2003sarkar@gmail.com>
2021-12-05 20:32:42 +0530

    Just testing

---
+++ Java_with_vs/testing.java
@@ -0,0 +1,13 @@
+class testing
+{
+    public static void main(String[] args)
+    {
+        for (int m = 5; m <= 20; m += 5) 
+        {
+            if ((m % 3) == 0)
+                break;
+            else if ((m % 5) == 0)
+                System.out.println(m);
+        }  
+    }
+}
