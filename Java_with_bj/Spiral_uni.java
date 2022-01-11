824e18ba5b92adef348fcabefaae7a4a0a15f6f9
Bhuttu03@ <sambit2003sarkar@gmail.com>
2022-01-11 21:49:55 +0530

    correcting main method

--- Java_with_bj/Spiral_uni.java
+++ Java_with_bj/Spiral_uni.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Spiral_uni
 {
-    public static void main()
+    public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the size of the matrix");
