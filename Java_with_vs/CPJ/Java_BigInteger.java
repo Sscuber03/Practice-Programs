00df2dd04c31646a747c395f2f8738470f19214d
Bhuttu03@ <sambit2003sarkar@gmail.com>
2022-01-12 22:07:13 +0530

    java practice

---
+++ Java_with_vs/CPJ/Java_BigInteger.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.math.*;
+
+
+public class Java_BigInteger
+{
+
+    public static void main(String[] args) 
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner sc=new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        BigInteger sum = a.add(b);
+        BigInteger prod = a.multiply(b);
+        System.out.println(sum);
+        System.out.println(prod);
+
+        sc.close();
+    }
+}
---
+++ Java_with_vs/CPJ/Java_Primality_Test.java
@@ -0,0 +1,16 @@
+import java.math.*;
+import java.util.*;
+
+public class Java_Primality_Test {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        BigInteger n = sc.nextBigInteger();
+        if(n.isProbablePrime(1))
+        System.out.println("prime");
+        else
+        System.out.println("not prime");
+
+        sc.close();
+    }
+}
