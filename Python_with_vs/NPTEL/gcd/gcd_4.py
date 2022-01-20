3450e30c5617a844f47360d4168395a9da460ee3
Bhuttu03@ <sambit2003sarkar@gmail.com>
2022-01-20 11:56:43 +0530

    corrected it

--- Python_with_vs/NPTEL/gcd/gcd_4.py
+++ Python_with_vs/NPTEL/gcd/gcd_4.py
@@ -2,12 +2,14 @@ def gcd(m,n):
     
     i = min(m,n)
     
+    
     while i>0:
         if (m%i) == 0 and (n%i) == 0:
             print (i)
+            break
         else:
             i=i-1
             
-gcd(633,21)
+gcd(63,21)
 
             
